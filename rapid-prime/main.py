76a51001b47838540a0f48e12f91b1614b6edcda
David Senack <david.senack@gmail.com>
2023-03-26 10:11:39 -0400

    Refactor main CLI logic

--- rapid-prime/main.py
+++ rapid-prime/main.py
@@ -26,24 +26,24 @@ from miller_rabin import miller_rabin_test
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Test if a number is prime')
-    parser.add_argument('number', type=int, help='Number to test')
+    parser.add_argument('--number', '-n', type=int, help='Number to test')
     parser.add_argument(
-        '-m', type=int, help='Specifies that the number is a Mersenne prime exponent (m in 2^m - 1)')
-    parser.add_argument('-k', type=int, default=10,
+        '--mersenne', '-m', type=int, help='Specifies that the number is a Mersenne prime exponent (m in 2^m - 1)')
+    parser.add_argument('--iterations', '-i', type=int, default=10,
                         help='Number of iterations. Seven iterations is generally considered sufficient')
     args = parser.parse_args()
 
     # If the user specified an exponent, calculate the number
-    if args.m:
-        args.number = 2 ** args.m - 1
+    if args.mersenne:
+        args.number = 2 ** args.mersenne - 1
 
     # Convert the number to a gmpy2 mpz object and test it
     candidate_num = gmpy2.mpz(args.number)
-    is_prime = miller_rabin_test(candidate_num, args.k)
+    is_prime = miller_rabin_test(candidate_num, args.iterations)
 
     # Print the result based on whether the user specified an exponent
-    if args.m:
-        print(f"M{args.m} is {'probably prime' if is_prime else 'not prime'}")
+    if args.mersenne:
+        print(f"M{args.mersenne} is {'probably prime' if is_prime else 'not prime'}")
     else:
         print(f"{candidate_num} is {'probably prime' if is_prime else 'not prime'}")
 
